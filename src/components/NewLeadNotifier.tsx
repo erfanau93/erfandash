da6baad78a3c0551ea50784dec66fe3fd5166c7c
erfanau93 <erfanau93@gmail.com>
2026-01-13 00:03:14 +1100

    chore: remove unused useMemo import

--- src/components/NewLeadNotifier.tsx
+++ src/components/NewLeadNotifier.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { supabase } from '../lib/supabase'
 import { playNewLeadSound } from '../lib/sounds'
 
